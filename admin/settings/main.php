f737274f8b228164ba3a207f930e39e9a5937883
Daniel <dan@vesmirnekure.cz>
2023-04-01 10:31:34 +0200

    Add check if API call is correct

--- admin/settings/main.php
+++ admin/settings/main.php
@@ -83,12 +83,49 @@ class KLEN_Ecomail_Main
 	public function apiFieldCallback()
 	{
 		$api_key = get_option( 'klen_api_key' );
+        $api_valid = get_option('klen_api_key_validation');
+
+        if (!empty($api_key) && empty($api_valid)) {
+
+            // Make a call to the Ecomail API to check if the API key is valid
+            $url = 'https://api2.ecomailapp.cz/lists';
+
+            $args = array(
+                'headers' => array(
+                    'Content-Type' => 'application/json',
+                    'key' => $api_key
+                ),
+                'timeout' => 10
+            );
+            $response = wp_remote_get($url, $args);
+
+            if (!is_wp_error($response)) {
+                $result = json_decode($response['body'], true);
+                if(!empty($result['message']) && $result['message'] == 'Wrong api key') {
+                    // API key is invalid
+                    update_option('klen_api_key_validation', false);
+                    $api_valid = false;
+                }
+                else {
+                    // API key is valid
+                    update_option('klen_api_key_validation', true);
+                    $api_valid = true;
+                }
+            } else {
+                // API call failed
+                add_settings_error('klen_api_key', 'klen_api_key_error', __('API call failed', 'klen_admin'));
+            }
+        }
 
 		echo '<input class="klen-input klen-input_main klen-input_main-api-key" type="text" name="klen_api_key" value="' . esc_attr__( $api_key, 'klen' ) . '" placeholder="' . __( 'ecomail-api-key', 'klen' ) . '" required>';
 
-		echo '<span class="icon icon_success"></span>';
+        if($api_valid === true) {
+            echo '<span class="icon icon_success"></span>';
+        }
+        else {
+            echo '<br><span class="klen__alert klen__alert_error" style="display: inline-block;">' . __( 'This API key is not valid, please check it again.', 'klen' ) . '</span>';
+        }
 
-		echo '<br><span class="klen__alert klen__alert_error" style="display: inline-block;">' . __( 'This API key is not valid, please check it again.', 'klen' ) . '</span>';
 	}
 
 	/**
