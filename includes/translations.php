c05cb0534236173d7e5820f842792518d2943652
Daniel <dan@vesmirnekure.cz>
2023-04-12 20:32:38 +0200

    Add Polylang translations

--- includes/translations.php
+++ includes/translations.php
@@ -23,7 +23,7 @@ class KLEN_Ecomail_Translations
     public function actions()
     {
         add_action('init', array($this, 'WpmlTranslations'));
-        //add_action('init', array($this, 'WpmlTranslations'));
+        add_action('init', array($this, 'PolylangTranslations'));
     }
 
     /**
@@ -45,6 +45,28 @@ class KLEN_Ecomail_Translations
         icl_register_string('klen', __('Success Message', 'klen'), get_option('klen_labels_success'));
         icl_register_string('klen', __('Error Message', 'klen'), get_option('klen_labels_error'));
         icl_register_string('klen', __('Warning Message', 'klen'), get_option('klen_labels_warning'));
+
+    }
+
+    /**
+     * Register Strings for Polylang
+     *
+     * @return void
+     */
+    public function PolylangTranslations()
+    {
+        if (!function_exists('pll_register_string')) {
+            return;
+        }
+
+        pll_register_string(__('Title', 'klen'), get_option('klen_labels_title'), 'klen');
+        pll_register_string(__('Description', 'klen'), get_option('klen_labels_desc'), 'klen');
+        pll_register_string(__('Label', 'klen'), get_option('klen_labels_label'), 'klen');
+        pll_register_string(__('Placeholder', 'klen'), get_option('klen_labels_placeholder'), 'klen');
+        pll_register_string(__('Button Text', 'klen'), get_option('klen_labels_button'), 'klen');
+        pll_register_string(__('Success Message', 'klen'), get_option('klen_labels_success'), 'klen');
+        pll_register_string(__('Error Message', 'klen'), get_option('klen_labels_error'), 'klen');
+        pll_register_string(__('Warning Message', 'klen'), get_option('klen_labels_warning'), 'klen');
 
     }
 
